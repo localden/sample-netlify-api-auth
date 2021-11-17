908a9f0ede89209f3c8d08416bc23438eab977de
Den Delimarsky (Work) <53200638+localden@users.noreply.github.com>
2021-11-17 15:58:39 -0800

    Switching to nuxt.

---
+++ gha/components/GitHubView.vue
@@ -0,0 +1,17 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <p>{{ state }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    state: {
+      type: String,
+      required: false,
+    },
+  }
+}
+</script>
---
+++ gha/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <GitHubView :state="state" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import GitHubView from "../components/GitHubView.vue"
+import { getSecrets, NetlifySecrets } from '@netlify/functions'
+
+export default {
+  name: 'App',
+  components: {
+    GitHubView
+  },
+  async data () {
+   let secrets: NetlifySecrets = {};
+   secrets = await getSecrets();
+
+   return {
+     state: secrets,
+   }
+ }
+}
+</script>
