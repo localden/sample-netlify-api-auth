7cab41952a8d1f89e9b2b1bf792a28164681c374
Den Delimarsky (Work) <53200638+localden@users.noreply.github.com>
2021-11-18 12:19:33 -0800

    Update GitHubView.vue

--- gha/components/GitHubView.vue
+++ gha/components/GitHubView.vue
@@ -14,61 +14,56 @@ export default {
   props: {
     jsonData: String,
   },
-  // mounted() {
-  //   this.methods.getData(this.props.jsonData());
-  // },
-  // methods: {
-  //   getData(jsonContent : string) {
-  //     var margin = {top: 10, right: 30, bottom: 30, left: 40},
-  //         width = 460 - margin.left - margin.right,
-  //         height = 400 - margin.top - margin.bottom;
+  mounted() {
+    this.methods.getData(this.props.jsonData());
+  },
+  methods: {
+    getData(jsonContent : string) {
+      var margin = {top: 30, right: 30, bottom: 70, left: 60},
+          width = 460 - margin.left - margin.right,
+          height = 400 - margin.top - margin.bottom;
 
-  //     // append the svg object to the body of the page
-  //     var svg = d3.select("#gh_contributions")
-  //       .append("svg")
-  //         .attr("width", width + margin.left + margin.right)
-  //         .attr("height", height + margin.top + margin.bottom)
-  //       .append("g")
-  //         .attr("transform",
-  //               "translate(" + margin.left + "," + margin.top + ")");
+      // append the svg object to the body of the page
+      var svg = d3.select("#my_dataviz")
+        .append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
                 
-  //     let data = d3.json(jsonContent);
+      let data = d3.json(jsonContent);
 
-  //     // X axis: scale and draw:
-  //     var x = d3.scaleLinear()
-  //         .domain([0, 1000])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-  //         .range([0, width]);
-  //     svg.append("g")
-  //         .attr("transform", "translate(0," + height + ")")
-  //         .call(d3.axisBottom(x));
+      // X axis
+      var x = d3.scaleBand()
+        .range([ 0, width ])
+        .domain(data.map(function(d) { return d.date; }))
+        .padding(0.2);
+      svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+          .attr("transform", "translate(-10,0)rotate(-45)")
+          .style("text-anchor", "end");
 
-  //     // set the parameters for the histogram
-  //     var histogram = d3.histogram()
-  //         .value(function(d) { return d.price; })   // I need to give the vector of value
-  //         .domain(x.domain())  // then the domain of the graphic
-  //         .thresholds(x.ticks(70)); // then the numbers of bins
+      // Add Y axis
+      var y = d3.scaleLinear()
+        .domain([0, 13000])
+        .range([ height, 0]);
+      svg.append("g")
+        .call(d3.axisLeft(y));
 
-  //     // And apply this function to data to get the bins
-  //     var bins = histogram(data);
-
-  //     // Y axis: scale and draw:
-  //     var y = d3.scaleLinear()
-  //         .range([height, 0]);
-  //         y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
-  //     svg.append("g")
-  //         .call(d3.axisLeft(y));
-
-  //     // append the bar rectangles to the svg element
-  //     svg.selectAll("rect")
-  //         .data(bins)
-  //         .enter()
-  //         .append("rect")
-  //           .attr("x", 1)
-  //           .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-  //           .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
-  //           .attr("height", function(d) { return height - y(d.length); })
-  //           .style("fill", "#69b3a2")
-  //   }
-  // }
+      // Bars
+      svg.selectAll("mybar")
+        .data(data)
+        .enter()
+        .append("rect")
+          .attr("x", function(d) { return x(d.date); })
+          .attr("y", function(d) { return y(d.contributionCount); })
+          .attr("width", x.bandwidth())
+          .attr("height", function(d) { return height - y(d.contributionCount); })
+          .attr("fill", "#69b3a2")
+    }
+  }
 };
 </script>
