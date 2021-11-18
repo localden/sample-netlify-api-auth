fb2083538761551a1ea433261bb29badd240f42b
Den Delimarsky (Work) <53200638+localden@users.noreply.github.com>
2021-11-18 12:29:20 -0800

    Update GitHubView.vue

--- gha/components/GitHubView.vue
+++ gha/components/GitHubView.vue
@@ -32,43 +32,46 @@ export default {
           .attr("transform",
                 "translate(" + margin.left + "," + margin.top + ")");
                 
-      d3.json(jsonContent , function(data) {
+      d3.json(jsonContent)
+        .then((data) => {
+          data.forEach(function(d) {                              
+              d.date = d.date;                         
+              d.contributionCount = +d.contributionCount;                                 
+          });
 
-      data.forEach(function(d) {                              
-          d.date = d.date;                         
-          d.contributionCount = +d.contributionCount;                                 
-      });
+          // X axis
+          var x = d3.scaleBand()
+            .range([ 0, width ])
+            .domain(data.map(function(d) { return d.date; }))
+            .padding(0.2);
+          svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x))
+            .selectAll("text")
+              .attr("transform", "translate(-10,0)rotate(-45)")
+              .style("text-anchor", "end");
 
-      // X axis
-      var x = d3.scaleBand()
-        .range([ 0, width ])
-        .domain(data.map(function(d) { return d.date; }))
-        .padding(0.2);
-      svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-          .attr("transform", "translate(-10,0)rotate(-45)")
-          .style("text-anchor", "end");
+          // Add Y axis
+          var y = d3.scaleLinear()
+            .domain([0, 13000])
+            .range([ height, 0]);
+          svg.append("g")
+            .call(d3.axisLeft(y));
 
-      // Add Y axis
-      var y = d3.scaleLinear()
-        .domain([0, 13000])
-        .range([ height, 0]);
-      svg.append("g")
-        .call(d3.axisLeft(y));
-
-      // Bars
-      svg.selectAll("mybar")
-        .data(data)
-        .enter()
-        .append("rect")
-          .attr("x", function(d) { return x(d.date); })
-          .attr("y", function(d) { return y(d.contributionCount); })
-          .attr("width", x.bandwidth())
-          .attr("height", function(d) { return height - y(d.contributionCount); })
-          .attr("fill", "#69b3a2")
-      });
+          // Bars
+          svg.selectAll("mybar")
+            .data(data)
+            .enter()
+            .append("rect")
+              .attr("x", function(d) { return x(d.date); })
+              .attr("y", function(d) { return y(d.contributionCount); })
+              .attr("width", x.bandwidth())
+              .attr("height", function(d) { return height - y(d.contributionCount); })
+              .attr("fill", "#69b3a2")
+              })
+        .catch((err) => {
+            // Handle err
+        });
     }
   }
 };
