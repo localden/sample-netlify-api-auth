026a3b93704731b0ccf146d012ed240148795975
Den Delimarsky (Work) <53200638+localden@users.noreply.github.com>
2021-11-18 15:42:21 -0800

    Update GitHubView.vue

--- gha/components/GitHubView.vue
+++ gha/components/GitHubView.vue
@@ -21,7 +21,7 @@ export default {
 
       // append the svg object to the body of the page
       var svg = d3
-        .select("#my_dataviz")
+        .select("#gh_contrib")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
