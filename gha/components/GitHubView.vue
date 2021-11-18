d6494285bf9ae5a3a94989d0ba6aa53c4ac036af
Den Delimarsky (Work) <53200638+localden@users.noreply.github.com>
2021-11-18 11:20:45 -0800

    Update GitHubView.vue

--- gha/components/GitHubView.vue
+++ gha/components/GitHubView.vue
@@ -14,10 +14,10 @@ export default {
     jsonData: String,
   },
   mounted() {
-    this.methods.getData(this.props.jsonData);
+    this.methods.getData(this.props.jsonData.getValue());
   },
   methods: {
-    getData(jsonContent : String) {
+    getData(jsonContent : string) {
       var margin = {top: 10, right: 30, bottom: 30, left: 40},
           width = 460 - margin.left - margin.right,
           height = 400 - margin.top - margin.bottom;
