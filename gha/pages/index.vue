a40163184efdfe0ee19591fd4a44a123e434f5ae
Den Delimarsky (Work) <53200638+localden@users.noreply.github.com>
2021-11-18 10:19:56 -0800

    Update index.vue

--- gha/pages/index.vue
+++ gha/pages/index.vue
@@ -4,32 +4,46 @@
 
 <script lang="ts">
 import { getSecrets, NetlifySecrets } from "@netlify/functions";
-import {Context} from '@nuxt/types';
+import { Context } from "@nuxt/types";
 
 export default {
-      async asyncData(context : Context) {
-        try
-        {
-          let secrets: NetlifySecrets = {};
-          secrets = await getSecrets();
-          if (secrets.gitHub)
-          {
-            return {
-              jsonData: JSON.stringify(secrets.gitHub?.isLoggedIn),
-            }
-          }
-          else
-          {
-            return {
-              jsonData: {error: "No GitHub token available."}
-            }
-          }
-        } catch (e) {
-          context.error(e)
-        }
-      },
-    created() {
-        console.log("Created!");
+  async asyncData(context: Context) {
+    try {
+      let secrets: NetlifySecrets = {};
+      secrets = await getSecrets();
+      if (secrets.gitHub) {
+        let contributions = await getContributions(secrets.gitHub?.bearerToken)
+        return {
+          jsonData: JSON.stringify(contributions),
+        };
+      } else {
+        return {
+          jsonData: { error: "No GitHub token available." },
+        };
+      }
+    } catch (e) {
+      context.error(e);
     }
-  }
+  },
+  created() {
+    console.log("Created!");
+  },
+};
+
+async function getContributions(token) {
+  const headers = {
+    Authorization: `bearer ${token}`,
+  };
+  const body = {
+    query:
+      "query {viewer {contributionsCollection {contributionCalendar {totalContributions}}}}",
+  };
+  const response = await fetch("https://api.github.com/graphql", {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: headers,
+  });
+  const data = await response.json();
+  return data;
+}
 </script>
