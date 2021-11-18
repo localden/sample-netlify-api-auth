5403d5179dcfa5512482626af7c58cc55a57790f
Den Delimarsky (Work) <53200638+localden@users.noreply.github.com>
2021-11-17 19:42:12 -0800

    Update index.vue

--- gha/pages/index.vue
+++ gha/pages/index.vue
@@ -7,13 +7,18 @@ import Vue from 'vue'
 import { Handler, getSecrets, NetlifySecrets } from "@netlify/functions";
 
 export default {
-      async asyncData() {
-        window.console.log ('Hello world');
-        console.log("DATA!");
-        let secrets: NetlifySecrets = {};
-        secrets = await getSecrets();
-        return {
-          jsonData: JSON.stringify(secrets),
+      async asyncData(context) {
+        try
+        {
+          window.console.log ('Hello world');
+          console.log("DATA!");
+          let secrets: NetlifySecrets = {};
+          secrets = await getSecrets();
+          return {
+            jsonData: JSON.stringify(secrets),
+          }
+        } catch (e) {
+          context.error(e) // Show the nuxt error page with the thrown error
         }
       },
     created() {
