a8a18b30c26a384a0f502938a9a93238f3780dba
Den Delimarsky (Work) <53200638+localden@users.noreply.github.com>
2021-11-18 11:16:16 -0800

    Update GitHubView.vue

--- gha/components/GitHubView.vue
+++ gha/components/GitHubView.vue
@@ -6,22 +6,6 @@
   </div>
 </template>
 
-<script>
-// set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 40},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-var svg = d3.select("#gh_contributions")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-</script>
-
 <script lang="ts">
 import * as d3 from "d3"
 
@@ -33,7 +17,20 @@ export default {
     this.methods.getData(this.props.jsonData);
   },
   methods: {
-    getData(jsonContent) {
+    getData(jsonContent : String) {
+      var margin = {top: 10, right: 30, bottom: 30, left: 40},
+          width = 460 - margin.left - margin.right,
+          height = 400 - margin.top - margin.bottom;
+
+      // append the svg object to the body of the page
+      var svg = d3.select("#gh_contributions")
+        .append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+                
       let data = d3.json(jsonContent);
 
       // X axis: scale and draw:
