5db1d5e5c7fd3b60194e1d6cefe6f0b99af2c811
Den Delimarsky (Work) <53200638+localden@users.noreply.github.com>
2021-11-18 16:24:29 -0800

    Update GitHubView.vue

--- gha/components/GitHubView.vue
+++ gha/components/GitHubView.vue
@@ -1,9 +1,9 @@
 <!-- Please remove this file from your project -->
 <template>
-<div>
-  <svg id="ghcontrib" width="800" height="800"></svg>
-  <p>{{ jsonData }}</p>
-</div>
+  <div>
+    <svg id="ghcontrib" width="800" height="800"></svg>
+    <p>{{ jsonData }}</p>
+  </div>
 </template>
 
 <script>
@@ -18,63 +18,81 @@ export default {
       var data = JSON.parse(jsonContent);
       console.log("PARSED!");
 
-    // set the dimensions and margins of the graph
-    const margin = {top: 30, right: 30, bottom: 70, left: 60},
+      // set the dimensions and margins of the graph
+      const margin = { top: 30, right: 30, bottom: 70, left: 60 },
         width = 800 - margin.left - margin.right,
         height = 800 - margin.top - margin.bottom;
 
-    // append the svg object to the body of the page
-    const svg = d3.select("#ghcontrib");
+      // append the svg object to the body of the page
+      const svg = d3.select("#ghcontrib");
 
-    var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+      var x = d3.scale.ordinal().rangeRoundBands([0, width], 0.05);
 
-    var y = d3.scale.linear().range([height, 0]);
+      var y = d3.scale.linear().range([height, 0]);
 
-    var xAxis = d3.svg.axis()
+      var xAxis = d3.svg
+        .axis()
         .scale(x)
         .orient("bottom")
         .tickFormat(d3.time.format("%Y-%m"));
 
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left")
-        .ticks(10);
+      var yAxis = d3.svg.axis().scale(y).orient("left").ticks(10);
 
-    x.domain(data.map(function(d) { return d.date; }));
-    y.domain([0, d3.max(data, function(d) { return d.contributionCount; })]);
+      x.domain(
+        data.map(function (d) {
+          return d.date;
+        })
+      );
+      y.domain([
+        0,
+        d3.max(data, function (d) {
+          return d.contributionCount;
+        }),
+      ]);
 
-    svg.append("g")
+      svg
+        .append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis)
-      .selectAll("text")
+        .selectAll("text")
         .style("text-anchor", "end")
         .attr("dx", "-.8em")
         .attr("dy", "-.55em")
-        .attr("transform", "rotate(-90)" );
+        .attr("transform", "rotate(-90)");
 
-    svg.append("g")
+      svg
+        .append("g")
         .attr("class", "y axis")
         .call(yAxis)
-      .append("text")
+        .append("text")
         .attr("transform", "rotate(-90)")
         .attr("y", 6)
         .attr("dy", ".71em")
         .style("text-anchor", "end")
         .text("Value ($)");
 
-    svg.selectAll("bar")
+      svg
+        .selectAll("bar")
         .data(data)
-      .enter().append("rect")
+        .enter()
+        .append("rect")
         .style("fill", "steelblue")
-        .attr("x", function(d) { return x(d.date); })
+        .attr("x", function (d) {
+          return x(d.date);
+        })
         .attr("width", x.rangeBand())
-        .attr("y", function(d) { return y(d.contributionCount); })
-        .attr("height", function(d) { return height - y(d.contributionCount); });
-
+        .attr("y", function (d) {
+          return y(d.contributionCount);
+        })
+        .attr("height", function (d) {
+          return height - y(d.contributionCount);
+        });
     },
     mounted() {
+      console.log("Mounted!")
       this.getData(this.jsonData);
+      console.log(this.jsonData);
     },
   },
 };
