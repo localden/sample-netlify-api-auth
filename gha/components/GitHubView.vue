7e6907b38704ec5fb610f75b9502198d19203016
Den Delimarsky (Work) <53200638+localden@users.noreply.github.com>
2021-11-18 16:10:01 -0800

    Update GitHubView.vue

--- gha/components/GitHubView.vue
+++ gha/components/GitHubView.vue
@@ -1,6 +1,9 @@
 <!-- Please remove this file from your project -->
 <template>
+<div>
   <svg id="ghcontrib"></svg>
+  <p>{{ jsonData }}</p>
+</div>
 </template>
 
 <script>
@@ -15,58 +18,49 @@ export default {
       var data = JSON.parse(jsonContent);
       console.log("PARSED!");
 
-      var margin = { top: 30, right: 30, bottom: 70, left: 60 },
+    // set the dimensions and margins of the graph
+    const margin = {top: 30, right: 30, bottom: 70, left: 60},
         width = 460 - margin.left - margin.right,
         height = 400 - margin.top - margin.bottom;
 
-      // append the svg object to the body of the page
-      var svg = d3.select("#ghcontrib")
+    // append the svg object to the body of the page
+    const svg = d3.select("#ghcontrib")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
 
       console.log(svg);
 
       // X axis
-      var x = d3
-        .scaleBand()
-        .range([0, width])
-        .domain(
-          data.map(function (d) {
-            return d.date;
-          })
-        )
+      const x = d3.scaleBand()
+        .range([ 0, width ])
+        .domain(data.map(d => d.date))
         .padding(0.2);
-
-      svg
-        .append("g")
-        .attr("transform", "translate(0," + height + ")")
+      svg.append("g")
+        .attr("transform", `translate(0, ${height})`)
         .call(d3.axisBottom(x))
         .selectAll("text")
-        .attr("transform", "translate(-10,0)rotate(-45)")
-        .style("text-anchor", "end");
+          .attr("transform", "translate(-10,0)rotate(-45)")
+          .style("text-anchor", "end");
 
       // Add Y axis
-      var y = d3.scaleLinear().domain([0, 100]).range([height, 0]);
-      svg.append("g").call(d3.axisLeft(y));
+      const y = d3.scaleLinear()
+        .domain([0, 13000])
+        .range([ height, 0]);
+      svg.append("g")
+        .call(d3.axisLeft(y));
 
-      svg
-        .selectAll("bars-style")
+      // Bars
+      svg.selectAll("mybar")
         .data(data)
-        .enter()
-        .append("rect")
-        .attr("x", function (d) {
-          return x(d.date);
-        })
-        .attr("y", function (d) {
-          return y(d.contributionCount);
-        })
-        .attr("width", x.bandwidth())
-        .attr("height", function (d) {
-          return height - y(d.contributionCount);
-        })
-        .attr("fill", "#69b3a2");
+        .join("rect")
+          .attr("x", d => x(d.date))
+          .attr("y", d => y(d.contributionCount))
+          .attr("width", x.bandwidth())
+          .attr("height", d => height - y(d.contributionCount))
+          .attr("fill", "#69b3a2")
+
     },
     mounted() {
       this.getData(this.jsonData);
